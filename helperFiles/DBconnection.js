0969c775d6496ee18de45bbb022b64a80d80057f
BakhshiMoeez <moeezbakhshi123456@gmail.com>
2023-06-07 21:21:00 +0500

    Deploying Mongodb

--- helperFiles/DBconnection.js
+++ helperFiles/DBconnection.js
@@ -4,7 +4,7 @@ dotenv.config({ path: "./config.env" });
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.DB_URL}/globeRamblersDB`);
+    await mongoose.connect(`${process.env.DB_URL}`);
     console.log("MongoDB Connected");
   } catch (err) {
     console.error(err.message);
