6de225b90fe91cfe24874cbd15f50ebd79981256
BakhshiMoeez <moeezbakhshi123456@gmail.com>
2023-06-17 19:45:25 +0500

    Functional updateion of tours

--- controllers/tourUpdateDelete.js
+++ controllers/tourUpdateDelete.js
@@ -18,47 +18,49 @@ exports.updateTour = async (req, res) => {
   try {
     connectDB();
 
-    // const tour = new Tour({
-    //   email: req.body.sellerEmail,
-    //   sellerProfilePic: req.body.sellerProfilePic,
-    //   title: req.body.title,
-    //   description: req.body.desc,
-    //   coverImage: req.body.coverImage,
-    //   basePrice: req.body.basePrice,
-    //   bronzePhotographyDesc: req.body.bronzePhotographyDesc,
-    //   bronzePhotographyPrice: req.body.bronzePhotographyPrice,
-    //   bronzeHotelDesc: req.body.bronzeHotelDesc,
-    //   bronzeHotelPrice: req.body.bronzeHotelPrice,
-    //   bronzeMealDesc: req.body.bronzeMealDesc,
-    //   bronzeMealPrice: req.body.bronzeMealPrice,
-    //   bronzeCarDesc: req.body.bronzeCarDesc,
-    //   bronzeCarPrice: req.body.bronzeCarPrice,
-    //   bronzeAddtInfo: req.body.bronzeAddtionalInfo,
-    //   silverPhotographyDesc: req.body.silverPhotographyDesc,
-    //   silverPhotographyPrice: req.body.silverPhotographyPrice,
-    //   silverHotelDesc: req.body.silverHotelDesc,
-    //   silverHotelPrice: req.body.silverHotelPrice,
-    //   silverMealDesc: req.body.silverMealDesc,
-    //   silverMealPrice: req.body.silverMealPrice,
-    //   silverCarDesc: req.body.silverCarDesc,
-    //   silverCarPrice: req.body.silverCarPrice,
-    //   silverAddInfo: req.body.silverAddtionalInfo,
-    //   goldPhotographyDesc: req.body.goldPhotographyDesc,
-    //   goldPhotographyPrice: req.body.goldPhotographyPrice,
-    //   goldHotelDesc: req.body.goldHotelDesc,
-    //   goldHotelPrice: req.body.goldHotelPrice,
-    //   goldMealDesc: req.body.goldMealDesc,
-    //   goldMealPrice: req.body.goldMealPrice,
-    //   goldCarDesc: req.body.goldCarDesc,
-    //   goldCarPrice: req.body.goldCarPrice,
-    //   goldAddInfo: req.body.goldAddtionalInfo,
-    // });
-    // const result = await Tour.findOneAndUpdate(
-    //   { _id: new ObjectId(req.params.email) },
-    //   tour
-    // );
+    const tour = {
+      source: req.body.source,
+      destination: req.body.destination,
+      email: req.body.sellerEmail,
+      sellerProfilePic: req.body.sellerProfilePic,
+      title: req.body.title,
+      description: req.body.description,
+      coverImage: req.body.coverImage,
+      basePrice: req.body.basePrice,
+      bronzePhotographyDesc: req.body.bronzePhotographyDesc,
+      bronzePhotographyPrice: req.body.bronzePhotographyPrice,
+      bronzeHotelDesc: req.body.bronzeHotelDesc,
+      bronzeHotelPrice: req.body.bronzeHotelPrice,
+      bronzeMealDesc: req.body.bronzeMealDesc,
+      bronzeMealPrice: req.body.bronzeMealPrice,
+      bronzeCarDesc: req.body.bronzeCarDesc,
+      bronzeCarPrice: req.body.bronzeCarPrice,
+      bronzeAddtInfo: req.body.bronzeAddInfo,
+      silverPhotographyDesc: req.body.silverPhotographyDesc,
+      silverPhotographyPrice: req.body.silverPhotographyPrice,
+      silverHotelDesc: req.body.silverHotelDesc,
+      silverHotelPrice: req.body.silverHotelPrice,
+      silverMealDesc: req.body.silverMealDesc,
+      silverMealPrice: req.body.silverMealPrice,
+      silverCarDesc: req.body.silverCarDesc,
+      silverCarPrice: req.body.silverCarPrice,
+      silverAddInfo: req.body.silverAddInfo,
+      goldPhotographyDesc: req.body.goldPhotographyDesc,
+      goldPhotographyPrice: req.body.goldPhotographyPrice,
+      goldHotelDesc: req.body.goldHotelDesc,
+      goldHotelPrice: req.body.goldHotelPrice,
+      goldMealDesc: req.body.goldMealDesc,
+      goldMealPrice: req.body.goldMealPrice,
+      goldCarDesc: req.body.goldCarDesc,
+      goldCarPrice: req.body.goldCarPrice,
+      goldAddInfo: req.body.goldAddInfo,
+    };
 
-    const result = req.params.email;
+    const result = await Tour.findOneAndUpdate(
+      { _id: new ObjectId(req.params.email) },
+      tour
+    );
+
     res.status(200).send(result);
   } catch (err) {
     console.log(err.message);
